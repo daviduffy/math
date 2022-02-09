1f109edc0b98c774177a1118a11568f4b9a624d0
David Duffy <daviduffy1@gmail.com>
2022-02-09 05:54:26 -0800

    first commit, cloned initial build from "webpack-base" repo

---
+++ src/components/SomeComponent.js
@@ -0,0 +1,6 @@
+import Style from '../scss/index.scss';
+
+export default {
+  name: 'SomeComponent',
+  template: `<div></div>`
+};
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+            },
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, 'src', 'index.html'),
+          to: path.join(__dirname, 'dist')
+        },
+      ],
+    }),
+  ],
+};
